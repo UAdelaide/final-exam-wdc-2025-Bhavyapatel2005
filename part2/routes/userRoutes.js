443ab262ed23f1524f0aae864ff032a6b240914f
Bhavyapatel2005 <a1923298@adelaide.edu.au>
2025-06-21 02:07:15 +0000

    [automatic commit] 2025-06-21 02:07:14 UTC 101.115.235.209

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
 
 // Added code
 router.post('/logout', (req, res) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
@@ -65,6 +65,5 @@ router.post('/logout', (req, res) => {
     res.json({ message: 'Logged out' });
   });
 });
-
 module.exports = router;
 
